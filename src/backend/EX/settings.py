8d6decc68577cb6345f17f9a1ac771fde9569dc7
Windson yang <wiwindson@outlook.com>
2024-07-11 18:46:01 +0800

    clean up comment

--- src/backend/EX/settings.py
+++ src/backend/EX/settings.py
@@ -117,13 +117,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EX.wsgi.application'
 
-
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -158,17 +151,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# django-allauth setup
 
 SITE_ID = 1
 
